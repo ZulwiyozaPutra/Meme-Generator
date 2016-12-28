7082b7e0201df7a17007fb6d1127f2d4e4a77de7
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-12-28 15:38:19 +0700

    New view controller

---
+++ Meme Generator/SavedMemeCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SavedMemeCollectionViewController.swift
+//  Meme Generator
+//
+//  Created by Zulwiyoza Putra on 12/28/16.
+//  Copyright © 2016 Zulwiyoza Putra. All rights reserved.
+//
+
+import Foundation
